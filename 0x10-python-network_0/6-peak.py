9db3d1fc169c786123c952d8e056ffbbf83cabd3
Ahmed Fouad LAGHA <107846932+ahmedfouadlagha@users.noreply.github.com>
2024-01-26 21:58:01 +0100

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""
-a function that finds a peak in a list of unsorted integers.
-"""
+"""Module 6-peak.py"""
+
 
 def find_peak(list_of_integers):
-    """
-    Return a peak in a list of unsorted integers
-    """
-    if len(list_of_integers) == 0:
+    """Return a peak in a list of unsorted integers."""
+    if list_of_integers == []:
         return None
-    if len(list_of_integers) == 1:
+
+    size = len(list_of_integers)
+    if size == 1:
         return list_of_integers[0]
-    if len(list_of_integers) == 2:
+    elif size == 2:
         return max(list_of_integers)
 
-    mid = int(len(list_of_integers) / 2)
+    mid = int(size / 2)
     peak = list_of_integers[mid]
     if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
         return peak
