0339bca1939daae99f28b41c76ba5ad5d00e7822
Ahmed Fouad LAGHA <107846932+ahmedfouadlagha@users.noreply.github.com>
2024-01-26 20:32:04 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     with request.urlopen("https://intranet.hbtn.io/status") as response:
         response = response.read()
         print("Body response:")
-        print(f"\t- type: {}",type(response))
-        print(f"\t- content: {}",response)
-        print(f"\t- utf8 content: {}",response.decode('utf-8'))
+        print("\t- type: ",type(response))
+        print("\t- content: ",response)
+        print("\t- utf8 content: ",response.decode('utf-8'))
