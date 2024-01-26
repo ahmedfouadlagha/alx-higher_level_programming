fd1c289e6c6092b02d1e6035556ef524acaf097c
Ahmed Fouad LAGHA <107846932+ahmedfouadlagha@users.noreply.github.com>
2024-01-26 20:39:32 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""
-Task 0:
-Fetch https://intranet.hbtn.io/status
+"""Fetches https://alx-intranet.hbtn.io/status."""
+import urllib.request
 
-0-hbtn_status.py
-"""
-from urllib import request
 
 if __name__ == "__main__":
-    req = request.Request('https://intranet.hbtn.io/status')
-    with request.urlopen(req) as response:
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
         
